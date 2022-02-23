b9655e0f881d1f4b59172ad304066df5ad01847a
karenefereyan <karenefereyan@gmail.com>
2022-02-23 13:20:16 -0500

    Starter code with assets, containers and more

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from 'react';
+
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from './components';
 import './App.css';
 
-function App() {
-  return (
-   <div>Attempting to code this project again by myself from scratch</div>
-  );
-}
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+);
 
 export default App;
