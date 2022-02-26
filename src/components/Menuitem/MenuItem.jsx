16425fc0d76b42406deeaa336be83e3f7e18ed9d
karenefereyan <karenefereyan@gmail.com>
2022-02-26 11:30:24 -0500

    Desktop specialmenu section completed

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+// import data from '../../constants';
 
 import './MenuItem.css';
 
-const MenuItem = () => (
-  <div>
-    MenuItem
+const MenuItem = ({ item : {title, tags, price}}) => (
+  <div className='app__specialmenu-section'>
+    <div className='app__specialmenu-section_head'>
+        <div>
+          <h3>{title}</h3>
+          </div>
+        <div className='app__specialmenu-section_head_dash'></div>
+        <div>
+          <p>{price}</p>
+        </div>
+    </div>
+    <div className='app__specialmenu-section_tags'>
+     <p className='p__opensans'>{tags}</p> 
+      </div>
   </div>
 );
 
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,10 +1,48 @@
 import React from 'react';
+import { SubHeading }  from '../../components';
+import { data, images }  from '../../constants';
+
+import { MenuItem  } from '../../components';
 
 import './SpecialMenu.css';
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className='app__specialmenu section__padding'>
+
+      <div className='app__specialmenu-intro'>
+          <SubHeading title = "Menu that fits your palat" />
+          <h1 className='headtext__cormorant'>Today's Special</h1>
+      </div>
+
+      <div className='app__specialmenu-content'>
+
+            <div className='app__specialmenu-content_wines'>
+                <h1 className='specialmenu-content_heading'>Wine & Beer</h1>
+                {
+                  data.wines.map((wine) => (
+                    <MenuItem item = {wine} />
+                  ))
+                }
+            </div>
+
+            <div className = "app__specialmenu-content_image">
+              <img src={images.menu} alt="" />
+            </div>
+
+            <div className='app__specialmenu-content_cocktails'>
+                <h1 className='specialmenu-content_heading'>Cocktails</h1>
+                {
+                  data.cocktails.map((cocktail) => (
+                    <MenuItem item = {cocktail} />
+                  ))
+                }
+
+            </div>
+      </div>
+
+      <div className='app__specialmenu-content_button'>
+              <button type="button" className='custom__button'>View More</button>
+      </div>
   </div>
 );
 
