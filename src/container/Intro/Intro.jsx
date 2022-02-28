0a4072f0aeee6adae54b4708669a70b98eb16c93
karenefereyan <karenefereyan@gmail.com>
2022-02-28 14:10:38 -0500

    Intro video section responsive

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import { meal }  from '../../constants';
+
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
 
 import './Intro.css';
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+
+  const [playVideo, setPlayVideo] = useState(false);
+  const videoRef = useRef();
+
+  const handleVideo = () => {
+
+    /** Dynamic previous value to set new value */
+        setPlayVideo((prevPlayVideo) => !prevPlayVideo);
+
+        if(playVideo){
+          videoRef.current.pause();
+        }
+        else{
+          videoRef.current.play();
+        }
+  };
+
+  return (
+    <div className='app__video'>
+         <video 
+            src = {meal}
+            ref = {videoRef}
+            type="video/mp4"
+            controls = {false}
+            loop
+            muted
+         />
+         <div className='app__video-overlay flex__center'>
+            <div className='app__video-overlay_circle flex__center' 
+            onClick = { handleVideo} >
+              {
+                playVideo 
+                ? (
+                  <BsPauseFill fontSize={30} color='#fff' />
+                ): 
+                (
+                <BsFillPlayFill fontSize={30} color='#fff' />
+                )
+              }
+              
+            </div>
+        </div>
+    </div>
+ 
+)
+}
 
 export default Intro;
